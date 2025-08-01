1ef45eac3269ec12e0e851990a3bc05cb822f357
Dimitri-99 <dimifopa2@gmail.com>
2025-08-01 12:38:46 +0200

    Ajout d'une fontions

--- JavaScript/Jeu/script.js
+++ JavaScript/Jeu/script.js
@@ -1,36 +1,38 @@
+
 const form = document.getElementById('guessForm');
 const guessInput = document.getElementById('guessInput');
 const resultDiv = document.getElementById('result');
 const resetButton = document.getElementById('resetButton');
 
 let randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
 
-form.addEventListener('submit', function(event) {event.preventDefault();
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
     const userGuess = parseInt(guessInput.value);
-    if (userGuess < 1 || userGuess > 100) 
-        {
-            resultDiv.textContent = "Veuillez entrer un nombre entre 1 et 100.";
-            return;
-        }
-        if (userGuess === randomNumber) 
-            {
-                resultDiv.textContent = "Bravo ! Vous avez deviné le nombre !";
-                resetButton.style.display = 'block';
-            }
-             else if (userGuess < randomNumber) 
-                {
-                    resultDiv.textContent = "Trop bas ! Essayez encore.";
-                }
-                 else {
-                    resultDiv.textContent = "Trop haut ! Essayez encore.";
-                }
-            });
+    if (userGuess < 1 || userGuess > 100) {
+        resultDiv.textContent = "Veuillez entrer un nombre entre 1 et 100.";
+        return;
+    }
+    attempts++;
+    if (userGuess === randomNumber) {
+        resultDiv.textContent = `Bravo ! Vous avez deviné le nombre en ${attempts} tentative(s)`;
+        resetButton.style.display = 'block';
+    } else if (userGuess < randomNumber) {
+        resultDiv.textContent = `Trop bas ! Essayez encore.\nTentative n°${attempts}`;
+    } else {
+        resultDiv.textContent = `Trop haut ! Essayez encore.\nTentative n°${attempts}`;
+    }
+});
                    
-resetButton.addEventListener('click', function() 
-{
+
+resetButton.addEventListener('click', function() {
     randomNumber = Math.floor(Math.random() * 100) + 1;
+    attempts = 0;
     guessInput.value = '';
     resultDiv.textContent = '';
     resetButton.style.display = 'none';
     guessInput.focus();
 });
+
